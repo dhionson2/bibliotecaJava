b4b4ace1041c4f984abceb04845074d10b52d3e5
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2022-11-30 20:41:10 -0300

    tirando a média de 4 notas e mandando imprimir no console

--- exercicios/src/arrays/exercicios.java
+++ exercicios/src/arrays/exercicios.java
@@ -5,12 +5,23 @@ import java.util.Arrays;
 public class exercicios {
 
 	public static void main(String[] args) {
-		double[] notasAlunoA = new double[3];
+		double[] notasAlunoA = new double[4];
 		notasAlunoA[0] = 10.0;
 		notasAlunoA[1] = 7.5;
 		notasAlunoA[2] = 3.9;
+		notasAlunoA[3] = 5.9;
 		
 		System.out.println(Arrays.toString(notasAlunoA));
+		
+		//somando as notas
+		double total = 0;		
+		for(int i = 0; i < notasAlunoA.length; i++) {
+			total += notasAlunoA[i];
+		}
+		//tirando a media das notas
+		double mediaNotas = total/notasAlunoA.length;
+		System.out.println("A média das notas é : "+mediaNotas);
+		
 	}
 	
 }
