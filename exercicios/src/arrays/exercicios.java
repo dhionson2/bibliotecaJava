4316dacc9665756617a92f55e57faf23f77caee4
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2022-11-30 20:30:22 -0300

    Primeiro exercicio criar e imprimir um array simples.

---
+++ exercicios/src/arrays/exercicios.java
@@ -0,0 +1,16 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class exercicios {
+
+	public static void main(String[] args) {
+		double[] notasAlunoA = new double[3];
+		notasAlunoA[0] = 10.0;
+		notasAlunoA[1] = 7.5;
+		notasAlunoA[2] = 3.9;
+		
+		System.out.println(Arrays.toString(notasAlunoA));
+	}
+	
+}
